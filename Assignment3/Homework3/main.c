90598e0255f93d2efbf38554c62cfc85593e8f41
jhoo08 <jamisonhs.08@gmail.com>
2019-10-26 14:29:25 -0700

    Adding Video

--- Assignment3/Homework3/main.c
+++ Assignment3/Homework3/main.c
@@ -1,5 +1,6 @@
 int main()
 {
+  
   *((unsigned int*)0x40023830) = 0x01; //Enable clock to GPIOA
   *((unsigned int*)0x40020000) = 0xA8000400; // Set GPIA to output
   int counter = 0;
@@ -45,8 +46,23 @@ int main()
     {
       counter++;
     }
+   // letter J (second dash)
+    
+    *((unsigned int*)0x40020014) = 0x20; //LEN ON
+    counter = 0;
+    while(counter < 6000000) //turn on
+    {
+      counter++;
+    }
+    *((unsigned int*)0x40020014) = 0x00; //LED OFF
+    counter = 0;
+    while (counter < 2000000) //turn off
+    {
+      counter++;
+    }
    // letter J (third dash)
-   
+    
+  
    *((unsigned int*)0x40020014) = 0x00; //LED OFF
     counter = 0;
     while (counter < 6000000) //turn off
